f590a7c3d8bc2b79b5f96daa1959a3fac23c55ec
Jonathan Markman <jmarkman446@gmail.com>
2018-05-02 15:51:06 -0400

    added logic for checking if article bookmark exists

--- app/src/main/java/com/jmarkman/hackernewsreader/ArticleDetail.java
+++ app/src/main/java/com/jmarkman/hackernewsreader/ArticleDetail.java
@@ -1,19 +1,24 @@
 package com.jmarkman.hackernewsreader;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.jmarkman.hackernewsreader.data.BookmarkContract;
 import com.jmarkman.hackernewsreader.data.DatabaseHelper;
 import com.jmarkman.hackernewsreader.databinding.ActivityArticleDetailBinding;
 import com.jmarkman.hackernewsreader.data.BookmarkContract.BookmarksEntry;
 
 public class ArticleDetail extends AppCompatActivity implements View.OnClickListener
 {
+    private String[] databaseCols = { BookmarksEntry._ID, BookmarksEntry.ARTICLE_TITLE, BookmarksEntry.SOURCE_URL, BookmarksEntry.ARTICLE_ID, BookmarksEntry.SUBMITTED_BY, BookmarksEntry.DATE_SUBMITTED };
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -53,14 +58,43 @@ public class ArticleDetail extends AppCompatActivity implements View.OnClickList
 
         Article article = getIntent().getParcelableExtra("Article");
 
-        ContentValues values = new ContentValues();
-        values.put(BookmarksEntry.ARTICLE_TITLE, article.articleTitle);
-        values.put(BookmarksEntry.SOURCE_URL, article.articleUrl);
-        values.put(BookmarksEntry.ARTICLE_ID, article.articleID);
-        values.put(BookmarksEntry.SUBMITTED_BY, article.submittedBy);
-        values.put(BookmarksEntry.DATE_SUBMITTED, article.submissionTime);
+        boolean articleInBookmarks = checkIfArticleInDatabase(helper, article.articleID);
 
-        database.insert(BookmarksEntry.TABLE_NAME, null, values);
+        if (articleInBookmarks)
+        {
+            ContentValues values = new ContentValues();
+            values.put(BookmarksEntry.ARTICLE_TITLE, article.articleTitle);
+            values.put(BookmarksEntry.SOURCE_URL, article.articleUrl);
+            values.put(BookmarksEntry.ARTICLE_ID, article.articleID);
+            values.put(BookmarksEntry.SUBMITTED_BY, article.submittedBy);
+            values.put(BookmarksEntry.DATE_SUBMITTED, article.submissionTime);
+
+            database.insert(BookmarksEntry.TABLE_NAME, null, values);
+            database.close();
+        }
+        else
+        {
+            Toast.makeText(this, "This article is already in your bookmarks", Toast.LENGTH_LONG).show();
+        }
+
+        database.close();
+    }
+
+    private boolean checkIfArticleInDatabase(DatabaseHelper helper, String articleID)
+    {
+        boolean articleInBookmarks = false;
+
+        SQLiteDatabase database = helper.getReadableDatabase();
+        Cursor cursor = database.query(BookmarksEntry.TABLE_NAME, databaseCols, BookmarksEntry.ARTICLE_ID + " LIKE " + articleID, null, null,null,null);
+
+        if (cursor.getCount() > 0)
+        {
+            articleInBookmarks = true;
+            cursor.close();
+        }
+
+        cursor.close();
+        return articleInBookmarks;
     }
 
     @Override
